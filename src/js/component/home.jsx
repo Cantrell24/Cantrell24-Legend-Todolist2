1e82a67b5e7dac5baa8b1de2dda3384403f45c89
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-02-19 19:44:16 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,77 +1,93 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [ inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
-	let backendURL = "https://playground.4geeks.com/apis/fake/todos/user/cantrell24"
-	const getTodos = () => {
-		fetch(backendURL)
-		.then(response => response.json())
-		.then(data => setTodos(data))
-		.catch(error => console.log(error))
-	}
-	const updateAPI = (newTodos) => {
-		let opt = {
-			method: "PUT",
-			"Content-Type": "application/json",
-			body: JSON.stringify(newTodos)
-		}
-		fetch(backendURL, opt)
-		.then(response => response.json())
-		.then (data => {
-			console.log(data)
-			getTodos()
-		})
-		.catch(error => console.log(error))
-	}
-	useEffect(() => {
-		getTodos()
-	}, [])
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  let backendURL =
+    "https://playground.4geeks.com/apis/fake/todos/user/cantrell24";
 
-	return (
-		
-		<div className="container">
-			<h1> todos </h1>
-			<ul className ="d-flex flex-column">
-				<li>
-					<input
-						type = "text"
-						onChange = {(e)=> setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-							  updateAPI([...todos, {label:inputValue, done:false}]);
-							  setInputValue("");
-							}
-						  }}
-						// onKeyDown ={(e) => e.key == "Enter" ? setTodos([
-						// 	...todos, inputValue
-						// ]) : null}
-						placeholder="What Needs To Be Done"></input>
-						
-				</li>
-				{todos.map((todo,index)=>{
-					return <li key= {index}>{todo.label}<i onClick={() =>
-						updateAPI(
-							todos.filter(
-								(t, currentIndex) =>
-									index != currentIndex
-							)
-						)
-				}class="fa-solid fa-trash-can"></i></li>
-				})}
-				{/* <li>Make the bed <i class="fa-thin fa-trash-can float-right"></i></li>
-				<li>Walk the dog <i class="fa-solid fa-trash-can float-right"></i></li>
-				<li>Pay taxes<i class="fa-solid fa-trash-can"></i></li>
-				<li>Pick the kids up<i class="fa-solid fa-trash-can"></i></li> */}
-				<div> {todos.length} Items left</div>
-			</ul>
-		</div>
-	);
+  const getTodos = () => {
+    fetch(backendURL)
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.log(error));
+  };
+
+  const updateAPI = async (newTodos) => {
+	console.log(newTodos)
+    let opt = {
+      method: "PUT",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(newTodos)
+    };
+    let response = await fetch(backendURL, opt);
+    if (!response.ok) {
+      return false;
+    } else {
+      let data = await response.json();
+      console.log(data);
+      return true;
+    }
+  };
+
+  const addTodo = async (e) => {
+    if (e.key === "Enter") {
+      let newTodos = todos.concat({ "done": false, "label": inputValue });
+	  
+      let result = await updateAPI(newTodos);
+      if (result) {
+        setInputValue("");
+        getTodos();
+      }
+    }
+  };
+
+  const deleteTodo = async (index) => {
+    let newTodos = todos.filter((item, currentIndex) => index != currentIndex);
+	
+    let result = await updateAPI(newTodos);
+    if (result) {
+      getTodos();
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1> todos </h1>
+      <ul className="d-flex flex-column">
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={(e) => addTodo(e)}
+            placeholder="What Needs To Be Done"
+          ></input>
+        </li>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              {todo.label}
+              <i
+                onClick={() => deleteTodo(index)}
+                class="fa-solid fa-trash-can"
+              ></i>
+            </li>
+          );
+        })}
+
+        <div> {todos.length} Items left</div>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
